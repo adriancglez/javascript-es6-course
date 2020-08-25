d869c37b6b46235d877917baae65c32b8a5cda4f
Adrian Gonzalez <yo.robot.1990@gmail.com>
2020-08-25 17:19:45 -0500

    Tema introductorio a JS - resolve #1

---
+++ 01_Temas_JavaScript/02_Variables/app.js
@@ -0,0 +1,17 @@
+// String, Number, Float, Boolean, Array, Object, Function...
+
+var name = "Adrián";
+console.log(name);
+console.log(`La variable ${name} es de tipo: ${typeof name}`);
+
+name = 55;
+console.log(name);
+console.log(`La variable ${name} es de tipo: ${typeof name}`);
+
+name = 109.7;
+console.log(name);
+console.log(`La variable ${name} es de tipo: ${typeof name}`);
+
+name = {};
+console.log(name);
+console.log(`La variable ${name} es de tipo: ${typeof name}`);
